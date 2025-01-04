4bcb7a45b6f625952e796ae79b76b93a16d90e73
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-01-03 19:19:12 -0600

    fix: fixes a bug in loading tailwind styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import SignUp from "./auth/SignUp.tsx";
 import Dashboard from "./page/dashboard/Dashboard.tsx";
 
 export default function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/log-in" element={<LogIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/log-in" element={<LogIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { nextui } = require('@nextui-org/theme');
+import { nextui } from "@nextui-org/theme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,19 +6,19 @@ export default {
     "./public/**/*.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js',
-    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js",
   ],
   theme: {
     extend: {
       height: {
-        '10s': '10svh'
+        "10s": "10svh",
       },
       gridTemplateRows: {
         layout: "auto 1fr auto",
-      }
+      },
     },
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
