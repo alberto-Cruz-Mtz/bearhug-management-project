52ef239a0e0b7cdedcb443c3b69950c59c3fd7c4
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-01-18 14:31:02 -0600

    fix: corrige el redireccionamiento del correo del usuario

--- src/auth/components/verified_email/components/CardVerifiedEmail.tsx
+++ src/auth/components/verified_email/components/CardVerifiedEmail.tsx
@@ -1,22 +1,35 @@
 import { CheckIcon } from "./../icon/CheckIcon.tsx";
 
 interface Props {
-  username: string
+  username: string;
 }
 
-export const CardVerifiedEmail = ({username}: Props) => {
+export const CardVerifiedEmail = ({ username }: Props) => {
+  const domain = username.substring(username.indexOf("@") + 1);
+
   return (
     <article className="w-[300px] shadow-xl px-6 py-8 text-center border-1 border-gray-400 rounded-lg">
       <header>
         <h6 className="font-bold text-2xl">Verificación de Correo</h6>
       </header>
       <div className="flex flex-col justify-center mt-3">
-        <CheckIcon/>
+        <CheckIcon />
         <p className="font-bold text-lg mt-5">¡Correo enviado!</p>
       </div>
-      <p className="text-sm text-gray-500 mt-3">Hemos enviado un correo de verificación a: <a href={`mailto:${username}`} target="blank" className="text-blue-700 font-bold">{username}</a></p>
-      <p className="text-sm text-gray-500 mt-3">Por favor, revisa tu bandeja de entrada y sigue las instrucciones para completar el proceso de verificación.</p>
+      <p className="text-sm text-gray-500 mt-3">
+        Hemos enviado un correo de verificación a:{" "}
+        <a
+          href={`https://www.${domain}`}
+          target="blank"
+          className="text-blue-700 font-bold"
+        >
+          {username}
+        </a>
+      </p>
+      <p className="text-sm text-gray-500 mt-3">
+        Por favor, revisa tu bandeja de entrada y sigue las instrucciones para
+        completar el proceso de verificación.
+      </p>
     </article>
-  )
-}
-
+  );
+};
